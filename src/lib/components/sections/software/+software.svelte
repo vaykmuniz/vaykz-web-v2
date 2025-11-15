7f9f6207b243563db830c2808d18b2d9d65691dc
vaykz <gabrielrasmuniz@gmail.com>
2025-11-15 17:20:31 -0300

    Refactor software section layout; enhance Saudades page structure and styling

--- src/lib/components/sections/software/+software.svelte
+++ src/lib/components/sections/software/+software.svelte
@@ -5,14 +5,15 @@
 <section class="flex w-full flex-col gap-3 p-5">
 	<h3 class="font-serif text-2xl">Software Development</h3>
 	{#each EXPERIENCES as experience}
-		<div
-			class="flex w-full flex-col text-start"
-		>
+		<div class="flex w-full flex-col text-start">
 			<div class="flex flex-col py-2">
-				<h4 class="font-serif">{experience.position} at <span class="font-semibold font-mono">{experience.company}</span></h4>
+				<h4>
+					<span class="font-mono font-semibold">{experience.position}</span> at
+					<span class="font-mono font-semibold">{experience.company}</span>
+				</h4>
 				<p class="text-sm uppercase">{experience.period}</p>
 			</div>
-			<p class="px-2">- {experience.description}</p>
+			<!--<p class="px-2">{experience.description}</p>-->
 		</div>
 	{/each}
 </section>
--- src/routes/saudades/+page.svelte
+++ src/routes/saudades/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import Navbar from '$lib/components/navbar/+navbar.svelte';
 </script>
 
-<section class="w-full h-full">
-    <h1>Saudades</h1>
-</section>
+<div class="h-full w-full bg-slate-600 text-sky-200">
+	<Navbar />
+	<section class="flex w-full flex-col gap-2 bg-slate-700 p-5">
+		<h1 class="font-serif text-2xl">Saudades Gamez</h1>
+		<p class="mt-2 italic">HERE IS THE GOOD PART!</p>
+	</section>
+</div>
