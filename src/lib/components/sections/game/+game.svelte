aa926d30cc9240be8302a166a7482e5aa63c38aa
vaykz <gabrielrasmuniz@gmail.com>
2025-11-17 23:24:43 -0300

    Add Godot web export files
    - Added web.pck for Godot web project packaging.
    - Added web.png as the icon for the web project.
    - Added web.wasm for WebAssembly support in the Godot web export.

--- src/lib/components/sections/game/+game.svelte
+++ src/lib/components/sections/game/+game.svelte
@@ -1,8 +1,6 @@
 <div class="w-full bg-slate-700 p-5">
 	<section class="flex flex-col gap-2">
 		<h3 class="font-serif text-2xl">Game Development</h3>
-		<p class="mt-2 italic">
-			HERE IS THE GOOD PART! 
-		</p>
+		<p class="mt-2 italic">HERE IS THE GOOD PART!</p>
 	</section>
 </div>
--- src/routes/saudades/+page.svelte
+++ src/routes/saudades/+page.svelte
@@ -8,4 +8,5 @@
 		<h1 class="font-serif text-2xl">Saudades Gamez</h1>
 		<p class="mt-2 italic">HERE IS THE GOOD PART!</p>
 	</section>
+	<iframe src="/godot/web.html" title='Tarot Game' class="w-full h-[600px] border-none" />
 </div>
