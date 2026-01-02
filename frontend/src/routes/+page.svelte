2bd9f10f1a6ae5a96781b22ffcf70cc91d8cc6c9
vaykz <gabrielrasmuniz@gmail.com>
2026-01-01 23:06:11 -0300

    reshaped act + knockback

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import Presentation from "$lib/components/sections/presentation/+presentation.svelte";
-	import Navbar from '$lib/components/navbar/+navbar.svelte';
-
 </script>
 
 <div class="flex flex-col h-full w-full items-center justify-start bg-linear-to-b/oklch from-slate-600 from-10% via-slate-600 via-80% to-indigo-200 to-150%">
-	<Navbar />
-	<Presentation />
+	aaa
 </div>
