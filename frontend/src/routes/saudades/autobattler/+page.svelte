bc8249c93369804b169402f0fd35e71a223e7424
vaykz <gabrielrasmuniz@gmail.com>
2026-01-17 21:47:35 -0300

    add mobile swipe

--- frontend/src/routes/saudades/autobattler/+page.svelte
+++ frontend/src/routes/saudades/autobattler/+page.svelte
@@ -4,5 +4,5 @@
 
 <div class="h-full w-full bg-slate-600 text-sky-200 flex flex-col items-center">
 	<Navbar />
-	<iframe src="/autobattler/autobattler.html" title='Auto battler game' class="w-fit h-[600px] border-none" > </iframe>
+	<iframe src="/autobattler/autobattler.html" title='Auto battler game' class="w-dvw h-150 border-none" > </iframe>
 </div>
