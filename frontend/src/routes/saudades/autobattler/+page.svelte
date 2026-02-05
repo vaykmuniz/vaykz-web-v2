f05ea62824cbbd384920f88e0baac0ea5b652de8
vaykz <gabrielrasmuniz@gmail.com>
2026-02-05 20:59:58 -0300

    game update v

--- frontend/src/routes/saudades/autobattler/+page.svelte
+++ frontend/src/routes/saudades/autobattler/+page.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div class="h-full w-full bg-slate-600 text-sky-200 flex flex-col items-center">
-	<iframe src="/autobattler/autobattler.html" title='Auto battler game' class="w-dvw h-150 border-none" > </iframe>
+	<iframe src="/autobattler/autobattler.html" title='Auto battler game' class="w-dvw h-full border-none" > </iframe>
 </div>
