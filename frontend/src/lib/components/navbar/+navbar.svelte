ee5fd9eaf30d242eadf849fd0e7865fc2259ff91
vaykz <gabrielrasmuniz@gmail.com>
2026-04-20 20:46:56 -0300

    fix: lang select baseline

--- frontend/src/lib/components/navbar/+navbar.svelte
+++ frontend/src/lib/components/navbar/+navbar.svelte
@@ -5,7 +5,7 @@
   const underline = "opacity-50 hover:opacity-100 relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-current after:transition-all hover:after:w-full pb-1";
 </script>
 
-<nav class="flex w-full px-4 md:px-10 py-2 items-center gap-2 justify-between sticky top-0 z-10 text-sky-200 text-lg">
+<nav class="flex w-full px-4 md:px-10 py-2 items-baseline gap-2 justify-between sticky top-0 z-10 text-sky-200 text-lg">
 	<div class="flex items-baseline gap-8 text-sm md:text-lg">
 		<img alt="my-cat-koji-icon" src={koji}  class="h-14"/>
 		<a href="/" class={underline}> .home </a>
