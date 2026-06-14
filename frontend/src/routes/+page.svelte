9d8b650e4f46f28a57ad33c2214a2f6863cfa114
Gabriel Muniz <gabrielrasmuniz@gmail.com>
2026-06-14 19:59:48 -0300

    feat: enhance animations and add new tween effects on page load

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { t } from '$lib/i18n/translations';
+	import { onMount } from 'svelte';
+	import { Tween } from 'svelte/motion';
 
 	$: TITLES = [$t("home.titles_1"), $t("home.titles_2"), $t("home.titles_3")];
 	$: ABOUT_ME_PARAGRAPHS = [
@@ -8,6 +10,18 @@
 		$t("home.about_me_description_3")
 	];
 
+	const nameOpacity = new Tween(0, { duration: 700 });
+	const titlesOpacity = new Tween(0, { duration: 700 });
+	const titlesX = new Tween(-100, { duration: 700 });
+	const aboutTitleOpacity = new Tween(0, { duration: 700 });
+	const aboutDescriptionOpacity = new Tween(0, { duration: 700 });
+
+	onMount(async () => {
+		await nameOpacity.set(0.5);
+		await Promise.all([titlesOpacity.set(1), titlesX.set(0)]);
+		await aboutTitleOpacity.set(1);
+		await aboutDescriptionOpacity.set(1);
+	});
 </script>
 
 
@@ -15,13 +29,18 @@
 
 	<div class="my-4 w-full flex flex-col items-start">
 		<h1
-			class="text-4xl opacity-50 uppercase font-thin tracking-widest font-oswald"
+			class="text-4xl uppercase font-thin tracking-widest font-oswald"
+			style:opacity={nameOpacity.current}
 		>
 			gabriel r. a. s. muniz
 		</h1>
 		<hr class="my-2 w-full md:w-1/4 h-px opacity-50"/>
 		
-		<div class="flex flex-col font-sans tracking-widest">
+		<div
+			class="flex flex-col font-sans tracking-widest"
+			style:opacity={titlesOpacity.current}
+			style:transform={`translateX(${titlesX.current}vw)`}
+		>
 			{#each TITLES as title}
 				<p>{title}</p>
 			{/each}
@@ -29,9 +48,14 @@
 	</div>
 
 	<div class="h-full w-full flex flex-col items-start">
-		<h2 class="text-2xl opacity-50 font-oswald font-thin uppercase tracking-widest">{$t("home.about_me_title")}</h2>
+		<h2
+			class="text-2xl font-oswald font-thin uppercase tracking-widest"
+			style:opacity={aboutTitleOpacity.current}
+		>
+			{$t("home.about_me_title")}
+		</h2>
 		<hr class="my-2 w-full md:w-1/4 h-px opacity-50"/>
-		<div class="w-full md:w-2/5 font-sans flex flex-col gap-4">
+		<div class="w-full md:w-2/5 font-sans flex flex-col gap-4" style:opacity={aboutDescriptionOpacity.current}>
 			{#each ABOUT_ME_PARAGRAPHS as paragraph}
 				<p>{paragraph}</p>
 			{/each}
